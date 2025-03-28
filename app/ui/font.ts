74742d42112b537b9103109dffb0e14c37e14937
Hafiz Caniago <hafizcode02@gmail.com>
2025-03-28 17:37:51 +0700

    feat: fix wrong import and add comment

--- app/ui/font.ts
+++ app/ui/font.ts
@@ -1,7 +1,10 @@
+// Adding Custom Font
 import { Inter, Lusitana } from "next/font/google";
 
+// Inter is a default font
 export const inter = Inter({ subsets: ["latin"] });
 
+// Custom Font
 export const lusitana = Lusitana({
   weight: ["400", "700"],
   subsets: ["latin"],
