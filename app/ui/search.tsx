2baa9181d38a1671031db26d11b7806145a0d8c1
Hafiz Caniago <hafizcode02@gmail.com>
2025-04-02 22:43:29 +0700

    feat: add debouce in search | prevent over query request

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,13 +2,15 @@
 
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { usePathname, useSearchParams, useRouter } from "next/navigation";
+import { useDebouncedCallback } from "use-debounce";
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams(); // get search params
 
   const pathName = usePathname(); // get current path
   const { replace } = useRouter();
-  function handleSearch(term: string) {
+
+  const handleSearch = useDebouncedCallback((term: string) => {
     const params = new URLSearchParams(searchParams);
     console.log(term);
     if (term) {
@@ -18,7 +20,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
     }
 
     replace(`${pathName}?${params.toString()}`); // update url
-  }
+  }, 500);
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
